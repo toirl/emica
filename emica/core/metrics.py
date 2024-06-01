5b89cfe11242efecd91993b4be5ffaed76d837d5
Torsten Irländer <torsten@irlaender.de>
2024-06-01 17:46:17 +0200

    Added metrics dataclass

---
+++ emica/core/metrics.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import List, Optional
+
+
+@dataclass
+class Metric:
+    timestamp: datetime
+    """Start date of observation as UTC datetime"""
+    duration: int
+    """Duration of observation in seconds"""
+    cpu_utilization: Optional[float] = None
+    """Utilisation of CPU in percent"""
+    cpu_energy: Optional[float] = None
+    """Energy used by the CPU in kWh"""
+    cpu_thermal_design_power: Optional[int] = None
+    """Thermal design power in Watt"""
+    memory_energy_per_gb: Optional[float] = None
+    """Energy used to for every used GB of memory in kWh"""
+    memory_utilization: Optional[float] = None
+    """Utilisation of memory in percent"""
+    memory_capacity: Optional[int] = None
+    """Total capacity of memory in GB"""
+    memory_energy: Optional[float] = None
+    """Energy used by the memory in kWh"""
+    device_emission_embodied: Optional[int] = None
+    """The total Life Cycle Assessment (LCA) emissions of the device in gCO2eg"""
+    device_expected_lifespan: Optional[int] = None
+    """The excepted lifespan from manufaturing to disposal in seconds"""
+    ressources_total: Optional[int] = None
+    """Number of total ressources (cpu) which can be used for operation"""
+    ressources_used: Optional[int] = None
+    """Number of actually rsed ressources (cpu) for operation"""
+    grid_carbon_intesity: Optional[int] = None
+    """The emitted carbon during operation in gCO2eg"""
+    funtional_unit: Optional[str] = None
+    """The functional unit in which to express the carbon impact. Must a name of a attribute present in the metrics dataset"""
+    funtional_unit_time: Optional[timedelta] = None
+    """The time to be used for functional unit conversions"""
+
+    def __post_init__(self):
+        if self.memory_energy_per_gb is None:
+            self.memory_energy_per_gb = 0.00039
+
+
+@dataclass
+class Metrics:
+    data: List[Metric]
