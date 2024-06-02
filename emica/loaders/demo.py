15d817607b8fea2b975786d20568a2236c6bc66f
Torsten Irländer <torsten@irlaender.de>
2024-06-02 22:26:11 +0200

    fix: Data in demoloader

--- emica/loaders/demo.py
+++ emica/loaders/demo.py
@@ -9,13 +9,13 @@ class DemoLoader:
             timestamp=datetime.now(tz=UTC),
             duration=60,
             cpu_thermal_design_power=20,
-            device_emission_embodied=167000,
+            device_emission_embodied=147000,
             device_expected_lifespan=94608000,  # 3 years in seconds
-            grid_carbon_intesity=434,  # gCO2/kWh according to Umweltbundesamt in 2022 for Germany
+            grid_carbon_intesity=500,  # gCO2/kWh according to Umweltbundesamt in 2022 for Germany
             funtional_unit="duration",
             funtional_unit_time=timedelta(minutes=1),
-            cpu_utilization=10.3,
-            memory_utilization=34.3,
+            cpu_utilization=8.791666666665066,
+            memory_utilization=26.267051696777344,
             memory_capacity=16,
         )
         return Metrics(data=[metric])
