f517ae7d11a6ea03e7ca0258014947229dcf2cea
Torsten Irländer <torsten@irlaender.de>
2024-06-02 21:44:00 +0200

    fix: Tests cpu_energy filter

--- tests/filters/test_cpu_energy.py
+++ tests/filters/test_cpu_energy.py
@@ -8,18 +8,22 @@ from emica.filters.cpu_energy import CPU2Energy
 
 def test_process():
     # Arrange
-    m = Metric(timestamp=datetime.now(tz=UTC), duration=60, cpu_thermal_design_power=40, cpu_utilization=10)
+    m = Metric(
+        timestamp=datetime.now(tz=UTC), duration=60, cpu_thermal_design_power=20, cpu_utilization=8.791666666665066
+    )
     f = CPU2Energy()
     # Act
     m = f.process(m)
     # Assert
-    assert m.cpu_energy == 4.0
+    assert m.cpu_energy == 0.00019839970774255318
 
 
 @pytest.mark.parametrize("attr", ["cpu_thermal_design_power", "cpu_utilization"])
 def test_fail_on_missing_attr(attr: str):
     # Arrange
-    m = Metric(timestamp=datetime.now(tz=UTC), duration=60, cpu_thermal_design_power=40, cpu_utilization=10)
+    m = Metric(
+        timestamp=datetime.now(tz=UTC), duration=60, cpu_thermal_design_power=20, cpu_utilization=8.791666666665066
+    )
     setattr(m, attr, None)
     f = CPU2Energy()
     # Act
